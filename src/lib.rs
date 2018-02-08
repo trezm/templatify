a36f6bc91822e8a31197c7be6644af92202a2327
Pete Mertz <peter.s.mertz@gmail.com>
2018-02-08 09:30:58 -0500

    Add gitignore

---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+#[macro_export]
+macro_rules! templatify {
+  ( $head_template:expr $(;$key:expr; $template:expr)* ) => {
+    {
+      let mut total_length = 0;
+      total_length = total_length + $head_template.len();
+
+      $(
+        total_length = total_length + $key.len() + $template.len();
+      )*
+
+      let mut output_string = String::with_capacity(total_length);
+      output_string.push_str($head_template);
+
+      $(
+        output_string.push_str($key);
+        output_string.push_str($template);
+      )*
+
+      output_string
+    }
+  }
+}
